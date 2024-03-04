ba930a1b582f38c384ad8b1dfbf045c4072e2db1
Renata Igarashi <rsigarashi@gmail.com>
2024-03-04 18:22:12 -0300

    feat: return all users in the repository

--- src/usecases/registerUserOnMailingList/repository/inMemoryUserRepository.ts
+++ src/usecases/registerUserOnMailingList/repository/inMemoryUserRepository.ts
@@ -26,7 +26,7 @@ export class InMemoryUserRepository implements IUserRepository {
   }
 
   async findAllUsers (): Promise<IUserData[]> {
-    throw new Error('Method not implemented')
+    return this.repository
   }
 
   async exists (user: IUserData): Promise<boolean> {
