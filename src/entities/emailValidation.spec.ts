8efd6f144c80ef643b1f46245827f6b5631342d3
Renata Igarashi <rsigarashi@gmail.com>
2024-03-04 20:35:12 -0300

    test: validade emails with more than 320 chars

--- src/entities/emailValidation.spec.ts
+++ src/entities/emailValidation.spec.ts
@@ -16,6 +16,11 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeTruthy()
   })
 
+  test('should not accept email strings larger than 320 chars', () => {
+    const email: string = 'c'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
   test('should not accept local part (email first part) larger than 64 chars', () => {
     const email: string = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
