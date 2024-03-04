7128b6d07ecad476242f31e55440ed523bb68c5c
Renata Igarashi <rsigarashi@gmail.com>
2024-03-04 18:00:47 -0300

    feat: user repository interface

---
+++ src/usecases/registerUserOnMailingList/ports/IUserRepository.ts
@@ -0,0 +1,8 @@
+import { IUserData } from '../../IUserData'
+
+export interface IUserRepository {
+  add(user: IUserData): Promise<void>
+  findUserByEmail(email: string): Promise<IUserData>
+  findAllUsers(): Promise<IUserData[]>
+  exists(user: IUserData): Promise<boolean>
+}
