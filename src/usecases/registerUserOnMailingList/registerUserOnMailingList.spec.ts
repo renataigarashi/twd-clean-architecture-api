67db51c9ccf29c3e9191e761c74271a06b76d707
Renata Igarashi <rsigarashi@gmail.com>
2024-03-04 17:33:05 -0300

    feat: user data interface

---
+++ src/usecases/registerUserOnMailingList/registerUserOnMailingList.spec.ts
@@ -0,0 +1,15 @@
+import { IUserData } from '../IUserData'
+
+describe('Register user on mailing list use case', () => {
+  test('Should add user with complete data to mailing list', () => {
+    const users: IUserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'Any_name'
+    // const email = 'any@mail.com'
+    // const response = await useCase.registerUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@mail.com')
+    // expect(await user).name.toBe('Any_name')
+  })
+})
