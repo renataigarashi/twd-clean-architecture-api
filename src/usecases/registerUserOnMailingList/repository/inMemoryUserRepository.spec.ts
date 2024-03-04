409976477299f3ceaa2e62dac247c6eb9e7d973c
Renata Igarashi <rsigarashi@gmail.com>
2024-03-04 18:14:59 -0300

    test: add user and checks it exists

--- src/usecases/registerUserOnMailingList/repository/inMemoryUserRepository.spec.ts
+++ src/usecases/registerUserOnMailingList/repository/inMemoryUserRepository.spec.ts
@@ -8,4 +8,15 @@ describe('In memory User repository', () => {
     const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user).toBeNull()
   })
+
+  test('shoud return user if it is found in the repository', async () => {
+    const users: IUserData[] = []
+    const name = 'any_name'
+    const email = 'any@mail.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+
+    const user = await userRepo.findUserByEmail('any@mail.com')
+    expect(user.name).toBe('any_name')
+  })
 })
