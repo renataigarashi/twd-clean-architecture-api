d8786cf4bc598d80d81593a34f444a1001bae565
Renata Igarashi <rsigarashi@gmail.com>
2024-03-05 12:27:27 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,4 +1,4 @@
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { IUserData } from './IUserData'
 import { Email } from './email'
 import { InvalidEmailError } from './errors/invalidEmailError'
@@ -6,15 +6,27 @@ import { InvalidNameError } from './errors/invalidNameError'
 import { Name } from './name'
 
 export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.email = email
+    this.name = name
+  }
+
   static create (userData: IUserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
     if (nameOrError.isLeft()) {
       return left(new InvalidNameError())
     }
     const emailOrError = Email.create(userData.email)
-
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
